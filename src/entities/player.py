09bfc6d54c8c5c6ff2f9fbe3b2cef349616473eb
Alexandr Novikov <a.novikov@dev.kokoc.com>
2025-10-26 17:33:48 +0300

    serialize player

--- src/entities/player.py
+++ src/entities/player.py
@@ -3,6 +3,19 @@ from src.entities.base import Actor, CharacterStats
 
 
 class Player(Actor):
-    def __init__(self, positon: Point, name: str, stats: CharacterStats):
-        super().__init__(position=positon, stats=stats)
+    def __init__(self, position: Point, name: str, stats: CharacterStats):
+        super().__init__(position=position, stats=stats)
         self.name = name
+
+    def to_dict(self):
+        return super().to_dict() | {"name": self.name}
+
+    @classmethod
+    def from_dict(cls, _dict):
+        return cls(
+            **{
+                "position": Point.from_dict(_dict["position"]),
+                "stats": CharacterStats.from_dict(_dict["stats"]),
+                "name": _dict["name"]
+            }
+        )
