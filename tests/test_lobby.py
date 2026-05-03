88810533e8bb5c8ffb7a7ade62cc48727638a558
Alexandr Novikov <a.novikov@dev.kokoc.com>
2026-05-03 10:25:53 +0300

    add tests

--- tests/test_lobby.py
+++ tests/test_lobby.py
@@ -34,5 +34,43 @@ def test_connect_to_nonexistent_lobby():
     assert response.json() == {"detail": "Lobby not found"}
 
 
+def test_join_lobby():
+    resp = client.post("/lobbies", json={
+        "players_num": 2,
+        "created_by_player_id": "123e4567-e89b-12d3-a456-426614174000"
+        }
+    )
+    response = client.post("/connect_lobby", json={
+            "lobby_id": resp.json()["lobby_id"],
+            "player": {"id": "123e4567-e89b-12d3-a456-426614174000"}
+        }
+    )
+    assert response.status_code == 200
+    assert response.json()["detail"] == "player connected"
+
+
+def test_connect_to_full_lobby():
+    # Create a lobby with 2 players
+    resp = client.post("/lobbies", json={
+        "players_num": 1,
+        "created_by_player_id": "123e4567-e89b-12d3-a456-426614174000"
+    })
+    lobby_id = resp.json()["lobby_id"]
+
+    # Connect the 1st player to fill the lobby
+    client.post("/connect_lobby", json={
+        "lobby_id": lobby_id,
+        "player": {"id": "123e4567-e89b-12d3-a456-426614174001"}
+    })
+
+    # Try to connect the 2nd player to the full lobby
+    response = client.post("/connect_lobby", json={
+        "lobby_id": lobby_id,
+        "player": {"id": "123e4567-e89b-12d3-a456-426614174002"}
+    })
+    assert response.status_code == 200
+    assert response.json()["detail"] == "lobby full"
+
+
 # Stop the server after tests are done
 server_process.terminate()
