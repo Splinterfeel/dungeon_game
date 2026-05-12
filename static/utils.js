29bc2998b63c9e7b8265d9ca66b0ddf41d73fa72
Alexander Novikov <a.novikov@dev.kokoc.com>
2026-05-12 18:33:06 +0300

    replace crypto random uuid with self written function (safety block in browsers)

--- static/utils.js
+++ static/utils.js
@@ -36,3 +36,8 @@ function normalize_map_value(value) {
   return value.trim();
 }
 
+function generateUUID() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
